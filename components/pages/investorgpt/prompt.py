16a679aa0604365d172fa97c5eb7b84253b4fa18
Parkil <alkain77@gmail.com>
2024-06-14 15:25:45 +0900

    investorGPT agent 초기화 로직을 0.1 -> 0.2 로 변경

--- components/pages/investorgpt/prompt.py
+++ components/pages/investorgpt/prompt.py
@@ -1,13 +1,13 @@
-from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder, PromptTemplate
+from langchain_core.prompts import PromptTemplate, ChatPromptTemplate, MessagesPlaceholder
 
 
-def find_agent_prompt() -> PromptTemplate:
+def find_react_agent_prompt() -> PromptTemplate:
     return PromptTemplate.from_template("""
         You are a hedge fund manager.
 
         You evaluate a company and provide your opinion and reasons why the stock is a buy or not.
 
-        Consider the performance of a stock, the company overview and the income statement.
+        Consider the performance of a stock, the company overview and the income statement and weekly equity.
 
         Be assertive in your judgement and recommend the stock or advise the user against it.
         
@@ -31,3 +31,26 @@ def find_agent_prompt() -> PromptTemplate:
         Question: {input}
         Thought:{agent_scratchpad}
     """)
+
+
+def find_open_ai_function_prompt():
+    # langchain 0.2 부터 사용 되는 create_openai_functions_agent, create_react_agent 를 사용 하려면
+    # prompt 에 agent_scratchpad 를 반드시 포함 시켜야 한다
+    return ChatPromptTemplate.from_messages(
+        [
+            (
+                "system",
+                """
+                You are a hedge fund manager.
+
+                You evaluate a company and provide your opinion and reasons why the stock is a buy or not.
+
+                Consider the performance of a stock, the company overview and the income statement.
+
+                Be assertive in your judgement and recommend the stock or advise the user against it.
+                """,
+            ),
+            ("human", "{companyName}"),
+            MessagesPlaceholder("agent_scratchpad"),
+        ]
+    )
--- pages/06_InvestorGPT.py
+++ pages/06_InvestorGPT.py
@@ -1,9 +1,9 @@
 import streamlit as st
-from langchain.agents import AgentExecutor, create_react_agent, AgentType, initialize_agent
-from langchain_core.messages import SystemMessage
+from langchain.agents import AgentExecutor, create_openai_functions_agent
 from langchain_openai import ChatOpenAI
 
 from components.langchain.init_llm import initialize_open_ai_llm
+from components.pages.investorgpt.prompt import find_open_ai_function_prompt
 from components.pages.investorgpt.tools.company_income_statement_tool import CompanyIncomeStatementTool
 from components.pages.investorgpt.tools.company_over_view_tool import CompanyOverViewTool
 from components.pages.investorgpt.tools.company_weekly_series_tool import CompanyTimeSeriesWeeklyTool
@@ -24,35 +24,32 @@ def init_agent() -> AgentExecutor:
         CompanyTimeSeriesWeeklyTool(),
     ]
 
-    # stock_agent = create_react_agent(llm=init_open_ai(), tools=tools, prompt=find_agent_prompt())
-    # return AgentExecutor(agent=stock_agent, tools=tools, verbose=True)
-    
-    # initialize_agent 가 폐기되고 create_react_agent 를 쓰라고는 하는데 prompt 설정에 대한 정확한 설명이 안나와 있어서 그런가
-    # create_react_agent 는 CompanyIncomeStatementTool, CompanyTimeSeriesWeeklyTool 이 작동이 안된다
-    # 현재(2024-06-13) 까지의 정보로 추측하기로는 initialize_agent는 tool을 실행하는 기준을 langchain 로직으로 실행했는데 
-    # react_agent는 tool 실행 기준을 LLM에 넘긴것으로 추정
-    return initialize_agent(
-        llm=init_open_ai(),
-        verbose=True,
-        agent=AgentType.OPENAI_FUNCTIONS,
-        tools=[
-            StockMarketSymbolSearchTool(),
-            CompanyOverViewTool(),
-            CompanyIncomeStatementTool(),
-            CompanyTimeSeriesWeeklyTool(),
-        ],
-        agent_kwargs={
-            "system_message": SystemMessage(content="""
-            You are a hedge fund manager.
+    stock_agent = create_openai_functions_agent(llm=init_open_ai(), tools=tools, prompt=find_open_ai_function_prompt())
+    return AgentExecutor(agent=stock_agent, tools=tools, verbose=True)
 
-            You evaluate a company and provide your opinion and reasons why the stock is a buy or not.
-
-            Consider the performance of a stock, the company overview and the income statement.
-
-            Be assertive in your judgement and recommend the stock or advise the user against it.
-            """)
-        }
-    )
+    # langchain 0.1 소스
+    # return initialize_agent(
+    #     llm=init_open_ai(),
+    #     verbose=True,
+    #     agent=AgentType.OPENAI_FUNCTIONS,
+    #     tools=[
+    #         StockMarketSymbolSearchTool(),
+    #         CompanyOverViewTool(),
+    #         CompanyIncomeStatementTool(),
+    #         CompanyTimeSeriesWeeklyTool(),
+    #     ],
+    #     agent_kwargs={
+    #         "system_message": SystemMessage(content="""
+    #         You are a hedge fund manager.
+    #
+    #         You evaluate a company and provide your opinion and reasons why the stock is a buy or not.
+    #
+    #         Consider the performance of a stock, the company overview and the income statement.
+    #
+    #         Be assertive in your judgement and recommend the stock or advise the user against it.
+    #         """)
+    #     }
+    # )
 
 
 st.set_page_config(
@@ -74,6 +71,6 @@ company = st.text_input("Write a name of the company you are interested on")
 
 if company:
     agent = init_agent()
-    result = agent.invoke({"input": company})
+    result = agent.invoke({"companyName": company})
 
     st.write(result['output'].replace('$', '\\$'))
